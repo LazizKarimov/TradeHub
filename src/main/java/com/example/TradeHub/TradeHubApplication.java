cdd6fa288eafa8a4e0fffa30e91e0ade7f9944f5
MacBook <https://github.com/LazizKarimov>
2024-02-09 18:09:06 +0300

    подключил к бд postgres

--- src/main/java/com/example/TradeHub/TradeHubApplication.java
+++ src/main/java/com/example/TradeHub/TradeHubApplication.java
@@ -9,5 +9,5 @@ public class TradeHubApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TradeHubApplication.class, args);
 	}
-
+	
 }
--- src/main/java/com/example/TradeHub/service/ProductService.java
+++ src/main/java/com/example/TradeHub/service/ProductService.java
@@ -1,48 +1,35 @@
 package com.example.TradeHub.service;
 
 import com.example.TradeHub.model.Product;
+import com.example.TradeHub.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class ProductService {
-     List<Product> productList = new ArrayList<>();
-    private long ID = 0;
+    private final ProductRepository productRepository;
 
-    {
-        productList.add(new Product(++ID,
-                "iPhone11",
-                "Smartphone with great camera",
-                500,
-                "New York",
-                "John Doe"));
-        productList.add(new Product(++ID,
-                "iPhone12",
-                "camera better than 11",
-                800,
-                "Los Angeles",
-                "Larisa Ivanova"));
+    public List<Product> listProducts(String title) {
+        if (title != null) return productRepository.findByTitle(title);
+        return productRepository.findAll();
     }
 
-
-    public List<Product> listProducts() { return productList; }
-
     public void saveProduct(Product product) {
-        product.setId(++ID);
-        productList.add(product);
+        log.info("Saving new {}", product);
+        productRepository.save(product);
     }
 
     public void deleteProduct(Long id) {
-        productList.removeIf(product -> product.getId().equals(id));
+        productRepository.deleteById(id);
     }
 
     public Product getProductById(Long id) {
-        for (Product product : productList) {
-            if (product.getId().equals(id)) return product;
-        }
-        return null;
+        return productRepository.findById(id).orElse(null);
     }
-
 }
