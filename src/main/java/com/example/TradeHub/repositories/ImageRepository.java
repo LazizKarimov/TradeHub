0aef371d0bae4c426aadbdc9c855a0990b48d284
MacBook <https://github.com/LazizKarimov>
2024-02-12 14:42:09 +0300

    добавил классы которые не вошли в коммит

---
+++ src/main/java/com/example/TradeHub/repositories/ImageRepository.java
@@ -0,0 +1,8 @@
+package com.example.TradeHub.repositories;
+
+
+import com.example.TradeHub.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
---
+++ src/main/java/com/example/TradeHub/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.TradeHub.repositories;
+
+
+import com.example.TradeHub.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByTitle(String title);
+}
